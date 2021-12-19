16bc94ea8805847ea50828da2d950d3fbf10902e
CrispenGari <Crispengari@gmail.com>
2021-12-19 22:18:22 +0200

    graphql-links@apollo

--- mobile/providers/GraphQLApolloProvider.tsx
+++ mobile/providers/GraphQLApolloProvider.tsx
@@ -1,8 +1,26 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
 import React from "react";
 
+const mlBackendLink = new HttpLink({
+  uri: `https://7fd9-102-176-222-102.ngrok.io/graphql`,
+});
+
+const javaBackendLink = new HttpLink({
+  uri: `https://a8b8-102-176-222-102.ngrok.io/graphql`,
+});
 const client = new ApolloClient({
-  uri: `https://51f2-102-176-222-102.ngrok.io/graphql`,
+  link: ApolloLink.split(
+    (operation) =>
+      (operation.getContext() as any).clientName === "java-backend",
+    javaBackendLink,
+    mlBackendLink
+  ),
   cache: new InMemoryCache(),
 });
 const GraphQLApolloProvider: React.FC = ({ children }) => {
